12f78316d37c39ee92ef8d56881982a604e5fc3a
Robin-2407 <robinsukhdeve@gmail.com>
2025-01-18 00:44:48 +0530

    Initial Commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      console.log("Error in user signup", error.message);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } catch (error) {
+    console.error("Error in signup: ", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+
+export const logout = (req, res) => {
+  res.send("logout route");
+};
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
+  connectDB();
+});
